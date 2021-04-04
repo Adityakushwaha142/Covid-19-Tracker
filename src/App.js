81e79e48a8c551fb5d6ca6775bfae077b28f915e
Aditya Kushwaha <kushwahaaditya251@gmail.com>
2021-04-04 18:01:54 +0530

    Covid-19 Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function App() {
+  const [countries, setCountries] = React.useState([]);
+  const url = "https://disease.sh/v3/covid-19/countries";
+  useEffect(() => {
+    async function fetchCountries() {
+      const req = await axios.get(url);
+      console.log(req);
+      console.log(req.data[0].country);
+      const res = JSON.parse(JSON.stringify(req));
+      console.log(res);
+      console.log("apple");
+    }
+    fetchCountries();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_header">
+        <h1>Lets build this covid-19 tracker</h1>
+        <FormControl className="app_dropdown">
+          {
+            <Select variant="outlined">
+              {countries.map((country) => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))}
+            </Select>
+          }
+        </FormControl>
+      </div>
     </div>
   );
 }
